9fec9e8287ac36783399ba2aee4d455ce757916a
liruinan <liruinan666@163.com>
2016-07-24 01:37:35 +0800

    java关于时间的转换

---
+++ code/src/java/Time.java
@@ -0,0 +1,138 @@
+package java;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by apple on 16/7/24.
+ */
+public class Time {
+    /**
+     * 英文简写（默认）如：2010-12-01
+     */
+    public static String FORMAT_SHORT = "yyyy-MM-dd";
+
+    /**
+     * 英文全称  如：2010-12-01 23:15:06
+     */
+    public static String FORMAT_LONG = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 精确到毫秒的完整时间    如：yyyy-MM-dd HH:mm:ss.S
+     */
+    public static String FORMAT_FULL = "yyyy-MM-dd HH:mm:ss.S";
+
+    /**
+     * 中文简写  如：2010年12月01日
+     */
+    public static String FORMAT_SHORT_CN = "yyyy年MM月dd";
+
+    /**
+     * 中文全称  如：2010年12月01日  23时15分06秒
+     */
+    public static String FORMAT_LONG_CN = "yyyy年MM月dd日  HH时mm分ss秒";
+
+    /**
+     * 精确到毫秒的完整中文时间
+     */
+    public static String FORMAT_FULL_CN = "yyyy年MM月dd日  HH时mm分ss秒SSS毫秒";
+
+
+    /**
+     * 获取时间戳
+     */
+    public static String getTimeString() {
+        SimpleDateFormat df = new SimpleDateFormat(FORMAT_FULL);
+        Calendar calendar = Calendar.getInstance();
+        return df.format(calendar.getTime());
+    }
+
+    /**
+     * 获取日期年份
+     * @param date 日期
+     * @return
+     */
+    public static String getYear(Date date) {
+        SimpleDateFormat df = new SimpleDateFormat(FORMAT_FULL);
+        Calendar calendar = Calendar.getInstance();
+        return df.format(date).substring(0, 4);
+    }
+    /**
+     * 功能描述：返回月
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回月份
+     */
+    public static int getMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) + 1;
+    }
+
+    /**
+     * 功能描述：返回日
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回日份
+     */
+    public static int getDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * 功能描述：返回小
+     *
+     * @param date
+     *            日期
+     * @return 返回小时
+     */
+    public static int getHour(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    /**
+     * 功能描述：返回分
+     *
+     * @param date
+     *            日期
+     * @return 返回分钟
+     */
+    public static int getMinute(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MINUTE);
+    }
+
+    /**
+     * 返回秒钟
+     *
+     * @param date
+     *            Date 日期
+     * @return 返回秒钟
+     */
+    public static int getSecond(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.SECOND);
+    }
+
+    /**
+     * 功能描述：返回毫
+     *
+     * @param date
+     *            日期
+     * @return 返回毫
+     */
+    public static long getMillis(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.getTimeInMillis();
+    }
+}
